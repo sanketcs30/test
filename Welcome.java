4fc2f1086215fe7efc89ba6d83b94b9f856857f0
sanketcs30 <sanketcs30@gmail.com>
2023-12-15 16:59:16 +0530

    modify Welcome.java file with demanding skill

--- Welcome.java
+++ Welcome.java
@@ -4,5 +4,6 @@ public class Welcome{
    public static void main(String args[]){
       System.out.println("Hello world...");
       System.out.println("Welcome to the world of Java...");
+      System.out.println("Java fullstack development is one of the most demanding skillset...");
    }
 }
