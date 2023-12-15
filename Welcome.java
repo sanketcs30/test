080cc9dd8d350164e869a067e5bbc69bb4c3eb1a
sanketcs30 <150379281+sanketcs30@users.noreply.github.com>
2023-12-15 13:42:28 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -3,5 +3,6 @@ import java.lang.*;
 public class Welcome{
    public static void main(String args[]){
       System.out.println("Hello world...");
+      System.out.println("Welcome to the world of Java...");
    }
 }
